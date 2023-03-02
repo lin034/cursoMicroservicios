8f564e97a25a3b5800c26f9355f814e5b90cdcbc
lalin <laloe_034@hotmail.com>
2023-03-02 16:23:58 -0600

    commit congiguracion eurekaServer

--- usuario-servicio/usuario-servicio/src/main/java/com/pildoraslin/usuarioservicio/UsuarioServicioApplication.java
+++ usuario-servicio/usuario-servicio/src/main/java/com/pildoraslin/usuarioservicio/UsuarioServicioApplication.java
@@ -2,10 +2,12 @@ package com.pildoraslin.usuarioservicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class UsuarioServicioApplication {
 
 	public static void main(String[] args) {

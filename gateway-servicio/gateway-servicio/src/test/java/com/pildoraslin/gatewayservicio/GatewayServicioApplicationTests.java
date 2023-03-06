ea5d5947a584cf2bbf338d63b50d1aa229eeb4f3
lalin <laloe_034@hotmail.com>
2023-03-05 20:18:20 -0600

    gateway configuraciones

---
+++ gateway-servicio/gateway-servicio/src/test/java/com/pildoraslin/gatewayservicio/GatewayServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pildoraslin.gatewayservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
